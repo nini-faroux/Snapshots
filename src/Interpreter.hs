e8803ac61c07d27ae6bc9e2f585c918a0aad9175
nini-faroux <nfarrel1@tcd.ie>
2019-03-06 15:03:43 +0000

    set the program's state

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -5,9 +5,19 @@ import Data.Map as M
 import Control.Monad.Trans.State
 import Control.Monad.Trans.Except 
 
-type Evaluator a = StateT Env (ExceptT String IO) a
+type Evaluator a = StateT PState (ExceptT String IO) a
 
-runEvaluator :: Evaluator a -> IO (Either String (a, Env))
-runEvaluator e = runExceptT $ runStateT e M.empty
+runEvaluator :: Evaluator a -> IO (Either String (a, PState))
+runEvaluator e = runExceptT $ runStateT e bootState
 
+type Instruction = (Statement, Either String (Name, Val))
+type Instructions = [Instruction]
 
+data PState = 
+    PState {
+        pEnv :: Env, 
+        instructs  :: Instructions
+    } deriving Show
+
+bootState :: PState 
+bootState = PState { pEnv = M.empty,  instructs = [] }
