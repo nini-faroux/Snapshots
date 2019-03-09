94906e127eb517cfdc3b9baafd530161db2f3738
nini-faroux <nfarrel1@tcd.ie>
2019-03-09 00:54:38 +0000

    change state rep, add print instruction

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -10,56 +10,66 @@ import Control.Monad.Trans.Except
 import Control.Monad.Except (throwError)
 import Control.Monad.IO.Class (liftIO)
 
-type Interpreter a = StateT PState (ExceptT String IO) a
+type Interpreter a = StateT PState (ExceptT IError IO) a
 
-runInterpreter :: Interpreter a -> IO (Either String (a, PState))
+runInterpreter :: Interpreter a -> IO (Either IError (a, PState))
 runInterpreter e = runExceptT $ runStateT e bootState
 
 runI :: Statement -> IO ()
 runI statement = void . runInterpreter $ execute statement
 
-type Instruction = (Statement, Either NotFound (Name, Val))
-type Instructions = [Instruction]
-data NotFound = NotFound deriving Show
+type Id = Maybe Int
+type StackPtr = Int
+type Instruction = (Id, Statement)
+type Stack = [Instruction]
+newtype IError = IError String deriving Show
 
 data PState = 
     PState {
-        pEnv :: Env, 
-        instructs :: Instructions
+        pEnv :: Env
+      , stack :: Stack
+      , sp :: StackPtr
     } deriving Show
 
 bootState :: PState 
-bootState = PState { pEnv = M.empty, instructs = [] }
+bootState = PState { 
+    pEnv = M.empty
+  , stack = [] 
+  , sp = 0
+  }
 
 execute :: Statement -> Interpreter () 
 execute stmt@(Assign name exp) = do
-    store stmt $ Right name
+    store stmt
     env <- gets pEnv 
+    ins <- gets stack
     val <- runR exp 
     modify (\s -> s { pEnv = M.insert name val env })
-    liftIO . putStrLn $ "- " ++ "Value " ++ show val ++ " assigned to variable " ++ show name
+    displayI name val
 
 execute (Sequence s1 s2) = do
     execute s1
     execute s2
 
-store :: Statement -> Either NotFound Name -> Interpreter ()
-store stmt varName = do
-   prevIns <- gets instructs
-   case varName of
-     Left NotFound -> storeVal stmt prevIns (Left NotFound)
-     Right name -> do
-       env <- gets pEnv
-       case lookupVar name env of
-         Nothing    -> storeVal stmt prevIns (Left NotFound)
-         Just value -> storeVal stmt prevIns (Right (name, value))
+execute (Print (Var name)) = do
+    env <- gets pEnv 
+    case lookupVar name env of 
+        Nothing -> liftIO $ putStrLn "Variable not found"
+        (Just val) -> liftIO . putStrLn $ "Variable " ++ show name ++ " = " ++ show val
+    
+displayI :: Name -> Val -> Interpreter () 
+displayI name val = do
+    env <- gets pEnv 
+    ins <- gets stack 
+    liftIO . print $ toList env
+    liftIO $ print ins 
+    liftIO . putStrLn $ "- " ++ "Value " ++ show val ++ " assigned to variable " ++ show name
 
-storeVal :: Statement -> Instructions -> Either NotFound (Name, Val) -> Interpreter ()
-storeVal st ins nmVal = 
-  case nmVal of 
-    Left NotFound -> updateIns $ ins ++ [(st, Left NotFound)]
-    Right tup     -> updateIns $ ins ++ [(st, Right tup)]
-  where updateIns ins = modify (\s -> s { instructs = ins })
+store :: Statement -> Interpreter ()
+store stmt = do
+    stk <- gets stack
+    cp <- gets sp
+    modify (\s -> s { sp = cp + 1, stack = stk ++ [(Just cp, stmt)] })
 
 runR :: Expr -> Interpreter Val
 runR exp = do
@@ -71,4 +81,4 @@ runR exp = do
 notFound :: String -> Interpreter a
 notFound errMsg = do
     liftIO . putStrLn $ "- " ++ errMsg
-    throwError errMsg
+    throwError (IError errMsg)
