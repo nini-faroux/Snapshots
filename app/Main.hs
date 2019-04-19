5f0017f0b0d0e49795921a20035a0d9e3d749ab7
nini-faroux <nfarrel1@tcd.ie>
2019-04-19 15:45:43 +0100

    stylish haskell

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import System.Environment 
-import Language
-import Interpreter
+import           Interpreter
+import           Language
+import           System.Environment
 
 main :: IO ()
 main = do
