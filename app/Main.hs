010452c2286e3047018b38f8ba2ebdde44ccf6db
nini-faroux <nfarrel1@tcd.ie>
2019-10-28 19:08:51 +0000

    handle bad filepath for program

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,17 @@ module Main where
 import           Interpreter (runI)
 import           Language (Statement)
 import           System.Environment (getArgs)
+import           Control.Exception (SomeException, catch)
 
 main :: IO ()
 main = do
   [filepath] <- getArgs
-  f <- readFile filepath
+  f <- getFile filepath
   let s = read f :: Statement
   runI s
+
+getFile :: FilePath -> IO String
+getFile fp = readFile fp `catch` (\e -> print (e :: SomeException) >> putStrLn loading >> readFile defaultFile) 
+    where 
+      defaultFile = "./test/TestWhile.test"
+      loading = "Loading default file"
