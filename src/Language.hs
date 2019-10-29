a31531b92d67515ff395f24c7c5eefd3da7fbe40
nini-faroux <nfarrel1@tcd.ie>
2019-10-29 18:05:08 +0000

    cleaner show instance

--- src/Language.hs
+++ src/Language.hs
@@ -6,7 +6,7 @@ module Language
     , Val(..)
     ) where
 
-import qualified Data.Map as M
+import qualified Data.Map as Map
 
 data Expr =
     Const Val
@@ -21,7 +21,7 @@ data Expr =
   | Lt Expr Expr
   | Eq Expr Expr
   | Not Expr
-  deriving (Eq, Show, Read)
+  deriving (Eq, Read)
 
 data Statement =
     Sequence Statement Statement
@@ -29,12 +29,33 @@ data Statement =
   | If Expr Statement Statement
   | Assign Name Expr
   | Print Expr
-  deriving (Show, Eq, Read)
+  deriving (Eq, Read)
 
-type Env = M.Map Name Val
+type Env  = Map.Map Name Val
 type Name = String
 
 data Val =
     I Int
   | B Bool
   deriving (Eq, Show, Read)
+
+instance Show Statement where 
+  show (Sequence s1 s2) = " (Sequence " ++ show s1 ++ show s2 ++ ")"
+  show (Assign name e1) = " (Assign " ++ name ++ " " ++ show e1 ++ ")" 
+  show (While e1 s1)    = " (While " ++ show e1 ++ show s1 ++ ")"
+  show (If e1 s1 s2)    = " (If " ++ show e1 ++ show s2 ++ show s2 ++ ")" 
+  show (Print e1)       = " (Print " ++ show e1 ++ ")"
+
+instance Show Expr where 
+  show (Const val) = "(Const " ++ show val ++ ")"
+  show (Var name)  = "(Var " ++ name ++ ") "
+  show (Add e1 e2) = "(Add " ++ show e1 ++ show e2 ++ ")" 
+  show (Sub e1 e2) = "(Sub " ++ show e1 ++ show e2 ++ ")"
+  show (Mul e1 e2) = "(Mul " ++ show e1 ++ show e2 ++ ")"
+  show (Div e1 e2) = "(Div " ++ show e1 ++ show e2 ++ ")"
+  show (And e1 e2) = "(And " ++ show e1 ++ show e2 ++ ")"
+  show (Or e1 e2)  = "(Or " ++ show e1 ++ show e2 ++ ")"
+  show (Gt e1 e2)  = "(Gt " ++ show e1 ++ show e2 ++ ")"
+  show (Lt e1 e2)  = "(Lt " ++ show e1 ++ show e2 ++ ")"
+  show (Eq e1 e2)  = "(Eq " ++ show e1 ++ show e2 ++ ")"
+  show (Not e1)    = "(Not " ++ show e1 ++ ")"
